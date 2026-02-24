4bafced1b24c8b75d1a9bc0e27d4ee75e64d3e00
nsantillan4 <65107387+nsantillan4@users.noreply.github.com>
2026-02-24 10:05:17 +0100

    Create print_your_name.py

---
+++ print_your_name.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Crear la ventana
+pantalla = turtle.Screen()
+pantalla.title("Texto con Turtle")
+
+# Crear la tortuga
+t = turtle.Turtle()
+t.speed(1)
+
+# Ocultar la tortuga
+t.hideturtle()
+
+# Escribir texto
+t.penup()
+t.goto(0, 0)
+t.write("Hola Mundo", align="center", font=("Arial", 24, "bold"))
+
+# Mantener la ventana abierta hasta hacer clic
+pantalla.exitonclick()
